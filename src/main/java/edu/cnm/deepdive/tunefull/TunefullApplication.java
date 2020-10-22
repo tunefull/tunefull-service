ef6e254a5b6a540ec7845d298b15344c6c4d6fb4
rfrech <rfrechette@cnm.edu>
2020-10-22 12:30:02 -0700

    Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/tunefull/TunefullApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.tunefull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunefullApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TunefullApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/tunefull/TunefullApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.tunefull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunefullApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
