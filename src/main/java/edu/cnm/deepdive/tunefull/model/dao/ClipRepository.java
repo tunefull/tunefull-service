dca418458149863f5af8897b3997531c861a1f48
lsteiner9 <lsteiner1@cnm.edu>
2020-11-15 13:27:19 -0700

    Updated repositories slightly based on endpoints.

--- src/main/java/edu/cnm/deepdive/tunefull/model/dao/ClipRepository.java
+++ src/main/java/edu/cnm/deepdive/tunefull/model/dao/ClipRepository.java
@@ -1,6 +1,8 @@
 package edu.cnm.deepdive.tunefull.model.dao;
 
 import edu.cnm.deepdive.tunefull.model.entity.Clip;
+import edu.cnm.deepdive.tunefull.model.entity.User;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,7 +15,12 @@ public interface ClipRepository extends JpaRepository<Clip, Long> {
   //Gets all clips ordered by most recent-less recent
   Optional<List<Clip>> getClipsByUserIsNotNullOrderByDateTimePostedDesc();
 
+  //Will this also be the one that we use when getting clips for users that
+  //are friends or follows?
   //Gets all clips for a particular user, ordered by most recent-less recent
   Optional<List<Clip>> getClipsByUserIdOrderByDateTimePostedDesc(Long id);
+
+  // or maybe that would be something like this:
+  Optional<List<Clip>> getClipsByUserIsInOrderByDateTimePostedDesc(Collection<User> users);
 
 }
