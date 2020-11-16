b42d439bc82d62be8d25fc2bf7a2761599e0711a
lsteiner9 <lsteiner1@cnm.edu>
2020-11-16 11:25:41 -0700

    Setting up security. Runs.

--- src/main/java/edu/cnm/deepdive/tunefull/TunefullApplication.java
+++ src/main/java/edu/cnm/deepdive/tunefull/TunefullApplication.java
@@ -2,7 +2,10 @@ package edu.cnm.deepdive.tunefull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
+@EnableHypermediaSupport(type = {HypermediaType.HAL})
 @SpringBootApplication
 public class TunefullApplication {
 
