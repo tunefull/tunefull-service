9cc9518f53be8e2f45d3ca44dc22c63371a1efea
lsteiner9 <lsteiner1@cnm.edu>
2020-11-15 17:12:29 -0700

    Some work on the service class.
    Can't really do much until the repository branch is merged.

--- src/main/java/edu/cnm/deepdive/tunefull/service/UserService.java
+++ src/main/java/edu/cnm/deepdive/tunefull/service/UserService.java
@@ -1,10 +1,17 @@
 package edu.cnm.deepdive.tunefull.service;
 
+import edu.cnm.deepdive.tunefull.model.entity.User;
+import java.util.Collection;
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements Converter<Jwt, UsernamePasswordAuthenticationToken> {
 
   //created in repository branch
   //private final UserRepository userRepository;
@@ -12,6 +19,32 @@ public class UserService {
   @Autowired
   public UserService() {
 
+  }
+
+  public User getOrCreate(String oauthKey, String username /*maybe also some others?*/) {
+    /*return repository.getByOauthKey(oauthKey)
+    .map((user) -> {
+    return repository.save(user);
+    })
+    .orElseGet(() -> {
+    User user = new User();
+    user.setOauthKey(oauthKey);
+    user.setUsername(username)
+    user.set some other stuff???
+    return repository.save(user);
+    });
+    */
+    return null;
+  }
+
+  @Override
+  public UsernamePasswordAuthenticationToken convert(Jwt jwt) {
+    Collection<SimpleGrantedAuthority> grants =
+        Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+//    return new UsernamePasswordAuthenticationToken(
+//        getOrCreate()
+//    );
+    return null;
   }
 
 }
